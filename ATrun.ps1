6d5d3812338ac1b3a242eca2e97e8dc0c423cb1a
West-Ben <benjamin.west.217@gmail.com>
2025-05-22 13:34:09 -0700

    Update ATrun.ps1

--- ATrun.ps1
+++ ATrun.ps1
@@ -2,13 +2,13 @@
 cd F:\AT
 
 # Run the first Python script (checkmarket.py) and capture its output
-$checkMarketOutput = & "C:/Users/benja/AppData/Local/Programs/Python/Python311/python.exe" "F:/AT/checkmarket.py"
+$checkMarketOutput = & "python" "F:/AT/checkmarket.py"
 
 # Check if the Python script's output is "True"
 if ($checkMarketOutput -eq "True") {
     # If True, run the momentum.py script
-    & "C:/Users/benja/AppData/Local/Programs/Python/Python311/python.exe" "F:/AT/momentum.py"
-    & "C:/Users/benja/AppData/Local/Programs/Python/Python311/python.exe" "F:/AT/momentum-live.py"
+    & "python" "F:/AT/momentum.py"
+    & "python" "F:/AT/momentum-live.py"
 } else {
     # Otherwise, print a message
     Write-Host "checkmarket.py did not return 'True'."
